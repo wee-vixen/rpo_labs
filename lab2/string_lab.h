177f961fd71406e23c3a7a2539ff0d6a58998391
wee-vixen <120771913+wee-vixen@users.noreply.github.com>
2022-12-17 10:10:43 +0300

    as an aid

---
+++ lab2/string_lab.h
@@ -0,0 +1,15 @@
+#ifndef STRING_LAB
+#define STRING_LAB
+
+char *_strremove(char *str, const char ch);
+int _strcountchar(const char *str, const char ch);
+char *_strdelspaces(char *str);
+char *_strtolower(char *str);
+char *_strtoupper(char *str);
+int _strcmp(const char *str1, const char *str2);
+char *_strreverse(const char *str1);
+int _atoi(const char *str);
+char *_strchr(char *str, int ch);
+char *_strstr(const char *str, const char *substr);
+
+#endif /*STRING_LAB*/
